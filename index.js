a86f7202d7f89cc362ad2c3c83d788a45d29f838
Gabriel Camaleonte <162625497+GabrielCamaleonte@users.noreply.github.com>
2024-08-08 17:43:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+var btnOpenModal = document.querySelectorAll('#btnCadaster');
+btnOpenModal.forEach(openE1 => openE1.addEventListener('click', openModal));
 
+function openModal(e){
+    var modalContainer = document.querySelector('#modalContainer');
+    modalContainer.classList.add('visivel');
+}
+
+var btnCloseModal = document.querySelector('#btnClose');
+btnCloseModal.addEventListener('click', closeModal);
+
+function closeModal(e){
+    var modalContainer = document.querySelector('#modalContainer');
+    modalContainer.classList.remove('visivel');
+}
